4b64cc03e082b9dfbbf65fbc53ffaed79491a885
Treesaken <mbilalfatiha62@gmail.com>
2025-01-24 14:31:43 +0700

    update backend dan frontend fitur

--- SI - KLINIK GIGI/backend/app.js
+++ SI - KLINIK GIGI/backend/app.js
@@ -6,17 +6,29 @@ const port = 3000;
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route untuk halaman utama
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+})
+
+// Route untuk halaman utama
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'index.html'));
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'booking.html'));
+app.get('/booking', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'booking.html'));
 });
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'data-dokter.html'));
+app.get('/datadokter', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'data-dokter.html'));
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'home.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'login.html'));
 });
 
 app.listen(port, () => {
